fe2dfac3704d7a64285a7973e8f43fd79a478b7b
manoti3 <hillarymanoti0@gmail.com>
2024-03-11 08:39:33 +0300

    completed assignment

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,42 @@
 class Game:
     def __init__(self, title):
         self.title = title
+        self._results = []
 
     def results(self):
-        pass
+        return self._results
 
     def players(self):
-        pass
+        return list(set(result.player for result in self._results))
 
     def average_score(self, player):
-        pass
+        player_results = [result.score for result in self._results if result.player == player]
+        if player_results:
+            return sum(player_results) / len(player_results)
+        else:
+            return 0
 
 class Player:
     def __init__(self, username):
         self.username = username
+        self._results = []
 
     def results(self):
-        pass
+        return self._results
 
     def games_played(self):
-        pass
+        return list(set(result.game for result in self._results))
 
     def played_game(self, game):
-        pass
+        return any(result.game == game for result in self._results)
 
     def num_times_played(self, game):
-        pass
+        return sum(result.game == game for result in self._results)
 
 class Result:
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        player.results().append(self)
+        game.results().append(self)
